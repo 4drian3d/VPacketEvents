3c0c3d1e3f412983b993a2017c123bc31a99939b
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-09-29 19:42:44 -0500

    publish: Setup publish plugin

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     `java-library`
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 dependencies {
@@ -20,64 +19,42 @@ tasks {
         (options as StandardJavadocDocletOptions).links("https://jd.papermc.io/velocity/3.4.0/")
     }
 }
-/*
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            repositories {
-                maven {
-                    credentials(PasswordCredentials::class)
-                    val central = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                    val snapshots = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
 
-                    if (project.version.toString().endsWith("SNAPSHOT")) {
-                        name = "SonatypeSnapshots"
-                        setUrl(snapshots)
-                    } else {
-                        name = "OSSRH"
-                        setUrl(central)
-                    }
-                }
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, "vpacketevents-api", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set("2022")
+        url.set("https://github.com/4drian3d/VPacketEvents")
+        licenses {
+            license {
+                name.set("GNU General Public License version 3 or later")
+                url.set("https://opensource.org/licenses/GPL-3.0")
             }
-            from(components["java"])
-            pom {
-                url.set("https://github.com/4drian3d/VPacketEvents")
-                licenses {
-                    license {
-                        name.set("GNU General Public License version 3 or later")
-                        url.set("https://opensource.org/licenses/GPL-3.0")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/4drian3d/VPacketEvents.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/4drian3d/VPacketEvents.git")
-                    url.set("https://github.com/4drian3d/VPacketEvents")
-                }
-                developers {
-                    developer {
-                        id.set("4drian3d")
-                        name.set("Adrian Gonzales")
-                        email.set("adriangonzalesval@gmail.com")
-                    }
-                }
-                issueManagement {
-                    name.set("GitHub")
-                    url.set("https://github.com/4drian3d/VPacketEvents/issues")
-                }
-                ciManagement {
-                    name.set("GitHub Actions")
-                    url.set("https://github.com/4drian3d/VPacketEvents/actions")
-                }
-                name.set(project.name)
-                description.set(project.description)
-                url.set("https://github.com/4drian3d/VPacketEvents")
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
             }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/4drian3d/VPacketEvents.git")
+            developerConnection.set("scm:git:ssh://git@github.com/4drian3d/VPacketEvents.git")
+            url.set("https://github.com/4drian3d/VPacketEvents")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/4drian3d/VPacketEvents")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/4drian3d/VPacketEvents/issues")
         }
     }
 }
-
-signing {
-    useGpgCmd()
-    sign(configurations.archives.get())
-    sign(publishing.publications["mavenJava"])
-}*/
