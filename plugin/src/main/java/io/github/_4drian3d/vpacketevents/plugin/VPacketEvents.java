8b6ed192fb7875a0b97d7aa9407f0eb874caf478
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-09-29 20:31:16 -0500

    fix: Improved limboapi compatibility
    What they do is horrible, but it costs me nothing to support them

--- plugin/src/main/java/io/github/_4drian3d/vpacketevents/plugin/VPacketEvents.java
+++ plugin/src/main/java/io/github/_4drian3d/vpacketevents/plugin/VPacketEvents.java
@@ -2,40 +2,37 @@ package io.github._4drian3d.vpacketevents.plugin;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.EventManager;
-import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.connection.LoginEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
 @SuppressWarnings("ClassCanBeRecord")
 @Plugin(
-        id = "vpacketevents",
-        name = "VPacketEvents",
-        description = "VelocityPacketEvents",
-        version = Constants.VERSION,
-        authors = {"4drian3d"},
-        // LimboAPI replaces the Velocity EventManager instance and makes the original instance unusable,
-        // so VPacketEvents is required to be initialized after LimboAPI
-        dependencies = {@Dependency(id = "limboapi", optional = true)}
+    id = "vpacketevents",
+    name = "VPacketEvents",
+    description = "Manage packets through Velocity native events",
+    version = Constants.VERSION,
+    authors = {"4drian3d"}
 )
 public final class VPacketEvents {
-    public static final String KEY = "vpacketevents";
-    private final EventManager eventManager;
-    private final Logger logger;
+  public static final String KEY = "vpacketevents";
+  private final ProxyServer proxyServer;
+  private final Logger logger;
 
-    @Inject
-    public VPacketEvents(final EventManager eventManager, final Logger logger) {
-        this.logger = logger;
-        this.eventManager = eventManager;
-    }
+  @Inject
+  public VPacketEvents(final ProxyServer proxyServer, final Logger logger) {
+    this.logger = logger;
+    this.proxyServer = proxyServer;
+  }
 
-    @Subscribe
-    public void onProxyInitialization(final ProxyInitializeEvent event) {
-        this.eventManager.register(this, LoginEvent.class, new LoginListener(eventManager, logger));
-        this.eventManager.register(this, DisconnectEvent.class, PostOrder.LAST, new DisconnectListener());
-    }
+  @Subscribe
+  public void onProxyInitialization(final ProxyInitializeEvent event) {
+    final EventManager eventManager = proxyServer.getEventManager();
+    eventManager.register(this, LoginEvent.class, new LoginListener(eventManager, logger));
+    eventManager.register(this, DisconnectEvent.class, (short) -404, new DisconnectListener());
+  }
 }
