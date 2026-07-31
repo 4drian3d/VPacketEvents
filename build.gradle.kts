4711fba05283d4efa1e2359aaff155e80d4d0ff6
Adrian Gonzales <adriangonzalesval@gmail.com>
2026-07-31 10:41:10 -0500

    misc: Updated to Velocity 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 subprojects {
-    apply<JavaPlugin>()
+    pluginManager.apply("java")
     repositories {
         maven("https://repo.papermc.io/repository/maven-public/")
-        maven("https://repo.opencollab.dev/maven-snapshots")
+        maven("https://maven.elytrium.net/repo/")
     }
     tasks {
         withType<JavaCompile> {
             options.encoding = Charsets.UTF_8.name()
-            options.release.set(21)
+            options.release.set(25)
         }
     }
     configure<JavaPluginExtension> {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+        toolchain.languageVersion.set(JavaLanguageVersion.of(25))
     }
 }
